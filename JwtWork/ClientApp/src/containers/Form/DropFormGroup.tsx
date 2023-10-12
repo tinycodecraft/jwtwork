2b9107b4fc3f2abadd2a4ee9f4919af6289b0946
tinycodecraft <raymond.cw.yau@gmail.com>
2023-10-12 11:45:31 +0800

    add gap and use tailwind material

--- JwtWork/ClientApp/src/containers/Form/DropFormGroup.tsx
+++ JwtWork/ClientApp/src/containers/Form/DropFormGroup.tsx
@@ -1,19 +1,21 @@
 import React, { useRef, useState, type FunctionComponent, useCallback } from 'react'
-import { Group, Text, useMantineTheme, rem, Image, SimpleGrid, Menu, Button, Anchor } from '@mantine/core'
+import { Group, Text, useMantineTheme, rem, Image, SimpleGrid } from '@mantine/core'
 import { IconUpload, IconPhoto, IconX } from '@tabler/icons-react'
 import { Dropzone, type DropzoneProps, MIME_TYPES, type FileWithPath } from '@mantine/dropzone'
 import { useAppDispatch, useAppSelector } from 'src/store'
 import { UploadStatusEnum, setUploadStatus, uploadFileAsync } from 'src/store/uploadSlice'
-import { range, useEventListener } from '@mantine/hooks'
+import { useEventListener } from '@mantine/hooks'
 import { canWait } from 'src/utils'
 import type { UploadedFileState } from 'src/fragments/types'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, Menu, MenuHandler, MenuItem, MenuList } from '@material-tailwind/react'
+import { ChevronDownIcon } from '@heroicons/react/24/outline'
 
 export const DropFormGroup: FunctionComponent = (props: Partial<DropzoneProps>) => {
   const theme = useMantineTheme()
   // use to invoke FileManager for upload i.e. openRef.current()
   const openRef = useRef<() => void>(null)
   const [files, setFiles] = useState<FileWithPath[]>([])
+  const [openMenu, setOpenMenu] = useState(false)
   const connectionId = useAppSelector<string | undefined>((state) => state.auth.connectionId)
   const uploadFiles = useAppSelector<UploadedFileState[] | undefined>((state) => state.file?.fileResults ?? [])
   const dropstatus = useAppSelector<UploadStatusEnum>((state) => state.file?.status ?? UploadStatusEnum.IDLE)
@@ -62,45 +64,34 @@ export const DropFormGroup: FunctionComponent = (props: Partial<DropzoneProps>) 
         <p className='level-item'>
           <h3 className='title is-4'>File Drop Zone</h3>
         </p>
-        <p className='level-item'>
+        <p className='level-item gap-1'>
           <a className='button is-success' {...{ disabled: !files || files.length == 0 || dropstatus !== UploadStatusEnum.IDLE }} ref={uploadRef}>
             Upload
           </a>
           {uploadFiles && dropstatus === UploadStatusEnum.SUCCESS && (
-            <Menu shadow='md' width={200} trigger='hover'>
-              <Menu.Target>
-                <Button
-                  variant='outline'
-                  leftIcon={<IconPhoto />}
-                  styles={(theme) => ({
-                    root: {
-                      backgroundColor: '#00acee',
-                      border: 0,
-                      height: rem(42),
-                      paddingLeft: rem(20),
-                      paddingRight: rem(20),
-                      '&:not([data-disabled])': theme.fn.hover({
-                        backgroundColor: theme.fn.darken('#beeafc', 0.05),
-                      }),
-                    },
-
-                    leftIcon: {
-                      marginRight: theme.spacing.md,
-                    },
-                  })}
-                >
-                  Files Uploaded
+            <Menu
+              animate={{
+                mount: { y: 0 },
+                unmount: { y: 25 },
+              }}
+              open={openMenu}
+              handler={setOpenMenu}
+              allowHover
+            >
+              <MenuHandler>
+                <Button size='sm' variant='outlined' className='flex items-center gap-3 text-sm font-normal' color='brown'>
+                  Your Uploaded{' '}<ChevronDownIcon strokeWidth={2.5} className={`h-3.5 w-3.5 transition-transform ${openMenu ? 'rotate-180' : ''}`} />
                 </Button>
-              </Menu.Target>
-              <Menu.Dropdown>
+              </MenuHandler>
+              <MenuList>
                 {uploadFiles.map((fi) => (
-                  <Menu.Item key={fi.filePath}>
-                    <Anchor href={fi.filePath} key={`ac-${fi.fileDesc}`}>
+                  <MenuItem key={fi.filePath}>
+                    <a href={fi.filePath} key={`ac-${fi.fileDesc}`}>
                       {fi.fileDesc}
-                    </Anchor>
-                  </Menu.Item>
+                    </a>{' '}
+                  </MenuItem>
                 ))}
-              </Menu.Dropdown>
+              </MenuList>
             </Menu>
           )}
           <a
