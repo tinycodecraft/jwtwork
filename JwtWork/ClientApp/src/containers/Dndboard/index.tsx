3879891c0d8b07cf049d4eca3e42111c03a394d1
tinycodecraft <raymond.cw.yau@gmail.com>
2023-11-03 10:46:14 +0800

    remove unwanted div

--- JwtWork/ClientApp/src/containers/Dndboard/index.tsx
+++ JwtWork/ClientApp/src/containers/Dndboard/index.tsx
@@ -6,12 +6,9 @@ import { ColorBoard } from './ColorBoard'
 const DndBoard: FunctionComponent = () => {
   const palletes = ['red', 'green', 'blue'].map((color) => ({ id: idGen(), color }))
   return (
-    <div>
-      <ColorPickProvider palleteplates={palletes} favor='#ddd' pick='#09C5D0'>
-        <ColorBoard />
-      </ColorPickProvider>
-    </div>
-
+    <ColorPickProvider palleteplates={palletes} favor='#ddd' pick='#09C5D0'>
+    <ColorBoard />
+  </ColorPickProvider>
     
   )
 }
